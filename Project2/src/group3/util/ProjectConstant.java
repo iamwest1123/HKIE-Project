4f14dfff51dcd55805010f7a018ff60d9b2ee6b9
Candy <candy.k.y.chan@oocl.com>
2016-10-14 10:21:02 +0800

    ProjectConstant for merchant status

--- Project2/src/group3/util/ProjectConstant.java
+++ Project2/src/group3/util/ProjectConstant.java
@@ -6,4 +6,9 @@ public class ProjectConstant {
 	public static final String DB_URL= "jdbc:oracle:thin:@ITA-031-W7.corp.oocl.com:1521:XE";
 	public static final String DB_DRIVER_CLASSNAME= "oracle.jdbc.OracleDriver";
 
+	public static final String MERCHANT_STATUS_ACCEPTED="Accepted";
+	public static final String MERCHANT_STATUS_REJECTED="Rejected";
+	public static final String MERCHANT_STATUS_FORZEN="Forzen";
+	public static final String MERCHANT_STATUS_UNDERREVIEW="UnderReview";
+	
 }
