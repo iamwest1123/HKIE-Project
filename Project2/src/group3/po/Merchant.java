f27435a2a083910386dbfd5c2edece02c8297447
Sunny W L Li <sunny.w.l.li@oocl.com>
2016-10-14 09:39:08 +0800

    Class field added

--- Project2/src/group3/po/Merchant.java
+++ Project2/src/group3/po/Merchant.java
@@ -1,5 +1,54 @@
 package group3.po;
 
+import java.sql.Date;
+
 public class Merchant {
 
+	
+	int id;
+	String name;
+	int age;
+	Date registerTime;
+	String address;
+	String shopPicUrl;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getRegisterTime() {
+		return registerTime;
+	}
+	public void setRegisterTime(Date registerTime) {
+		this.registerTime = registerTime;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getShopPicUrl() {
+		return shopPicUrl;
+	}
+	public void setShopPicUrl(String shopPicUrl) {
+		this.shopPicUrl = shopPicUrl;
+	}
+	
+	
+	
 }
