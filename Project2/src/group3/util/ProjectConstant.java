9fc2fc3bea7e0e792e9ff21edf55c49118c13cbd
Matthew Ma <MAMA4@ZHA-ITA076-W7.corp.oocl.com>
2016-10-14 11:16:26 +0800

    update constant

--- Project2/src/group3/util/ProjectConstant.java
+++ Project2/src/group3/util/ProjectConstant.java
@@ -6,4 +6,10 @@ public class ProjectConstant {
 	public static final String DB_URL= "jdbc:oracle:thin:@ITA-031-W7.corp.oocl.com:1521:XE";
 	public static final String DB_DRIVER_CLASSNAME= "oracle.jdbc.OracleDriver";
 
+	public static final String USERTYPE_MERCHENT = "MERCHENT";
+	public static final String USERTYPE_ADMIN = "ADMIN";
+	public static final String USERTYPE_CUSTOMER = "CUSTOMER";
+
+	public static final String UI_LOGIN = "login.jsp";
+	public static final String UI_LOGIN_AS = "loginAs.jsp";
 }
