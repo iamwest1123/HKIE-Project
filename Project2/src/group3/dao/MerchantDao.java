d2969111ff5ee92f1e10b22f415d49ac96c9bca1
Ken Siu <ken.siu@oocl.com>
2016-10-14 09:49:53 +0800

    update MerchantDao basic method

--- Project2/src/group3/dao/MerchantDao.java
+++ Project2/src/group3/dao/MerchantDao.java
@@ -1,5 +1,14 @@
 package group3.dao;
 
+import java.util.List;
+import group3.po.Merchant;
+
 public interface MerchantDao {
 
+	public void addMerchant(Merchant m);
+	public void updateMerchant(Merchant m);
+	public void deleteMerchant(int id);
+	public Merchant findMerchant();
+	public List<Merchant> findAllMerchant();
+	
 }
