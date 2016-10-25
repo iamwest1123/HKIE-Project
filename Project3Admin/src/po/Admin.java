4f38c11d8d6d221d6111116b4ccc14de5502d738
Sunny W L Li <sunny.w.l.li@oocl.com>
2016-10-25 18:01:45 +0800

    Admin Class added

---
+++ Project3Admin/src/po/Admin.java
@@ -0,0 +1,60 @@
+package po;
+
+public class Admin {
+	String password;
+	String loginName;
+	String id;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((loginName == null) ? 0 : loginName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin other = (Admin) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (loginName == null) {
+			if (other.loginName != null)
+				return false;
+		} else if (!loginName.equals(other.loginName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+}
---
+++ Project3Admin/src/po/AdminAdvert.java
@@ -0,0 +1,12 @@
+package po;
+
+import java.util.Date;
+
+public class AdminAdvert {
+	String merchantId;
+	String status;	//(active/pending/frozen)
+	// advance
+	double balance;
+	Date startDate;
+	Date expireDate;
+}
