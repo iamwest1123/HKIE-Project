2f7a81e7eb6edd21fb33ed2b0089ea56d3b6b323
Matthew Ma <matthew.ma@oocl.com>
2016-10-26 10:08:14 +0800

    CustomerOrderDao

--- Project3Customer/src/dao/impl/CustomerOrderDaoImpl.java
+++ Project3Customer/src/dao/impl/CustomerOrderDaoImpl.java
@@ -6,7 +6,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import dao.CustomerOrderDao;
+import po.Customer;
 import po.CustomerOrder;
+import po.Merchant;
+import util.ProjectConstant;
 
 public class CustomerOrderDaoImpl implements CustomerOrderDao{
 
@@ -23,6 +26,12 @@ public class CustomerOrderDaoImpl implements CustomerOrderDao{
 	public boolean updateOrder(CustomerOrder o) {
 		CustomerOrder o2 = em.getReference(CustomerOrder.class, o.getId());
 		// TODO copy o to o2
+		o2.setComment(o.getComment());
+		o2.setDishes(o.getDishes());
+		o2.setReceiveDate(o.getReceiveDate());
+		o2.setDeliverDate(o.getDeliverDate());
+		o2.setOrderDate(o.getOrderDate());
+		o2.setStatus(o.getStatus());
 		em.persist(o2);
 		return false;
 	}
@@ -30,27 +39,25 @@ public class CustomerOrderDaoImpl implements CustomerOrderDao{
 	@Override
 	public boolean deleteOrder(CustomerOrder o) {
 		CustomerOrder o2 = em.getReference(CustomerOrder.class, o.getId());
-		o2.setStatus(status);
+		o2.setStatus(ProjectConstant.DELIVERY_STATUS_CANCELED);
 		em.persist(o2);
 		return false;
 	}
 
 	@Override
-	public boolean deleteOrder(String id) {
-		// TODO Auto-generated method stub
-		return false;
+	public List<CustomerOrder> findAllByCustomer(Customer c) {
+		return em.createQuery("select o from CustomerOrder o")
+//				.setParameter(arg0, arg1)
+				.setMaxResults(50)
+				.getResultList();
 	}
 
 	@Override
-	public List<CustomerOrder> findAllByCustomer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<CustomerOrder> findAllByMerchant() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<CustomerOrder> findAllByMerchant(Merchant m) {
+		return em.createQuery("select o from CustomerOrder o")
+//				.setParameter(arg0, arg1)
+				.setMaxResults(50)
+				.getResultList();
 	}
 	
 
