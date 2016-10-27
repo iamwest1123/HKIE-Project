e2140177c80661361b26cf719a93aa9fe759bd63
Matthew Ma <matthew.ma@oocl.com>
2016-10-28 00:26:19 +0800

    persist object to admin db

--- Project3Admin/src/mq/impl/AdvertApplicationConsumer.java
+++ Project3Admin/src/mq/impl/AdvertApplicationConsumer.java
@@ -5,21 +5,29 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import dao.AdminAdvertDao;
 import dto.AdvertApplicationDto;
 import mq.QueueConsumer;
+import po.AdminAdvert;
 
 public class AdvertApplicationConsumer implements MessageListener,QueueConsumer {
-
+	@Autowired
+	private AdminAdvertDao adao;
+	
 	@Override
 	public void onMessage(Message arg0) {
 		TextMessage textMsg = (TextMessage) arg0;
 		try {
 			AdvertApplicationDto dto = (AdvertApplicationDto) convertStringToObject(textMsg.getText());
 			if (dto!=null) {
-				// TODO: persist data to admin db
 				System.out.println(dto.getMerchantId());
+				AdminAdvert aa = new AdminAdvert(); 
+				aa.setId(dto.getMerchantId());
+				adao.addAdminAdv(aa);
 			}
 		} catch (JMSException e) {
 			e.printStackTrace();
