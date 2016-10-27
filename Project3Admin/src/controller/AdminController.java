d6b3f7fe96bdbd11521762ee2d1e038aab5234c3
Ken Siu <ken.siu@oocl.com>
2016-10-28 00:40:27 +0800

    fix admin show merchant list status( should get from admin status table instead of merchant status)

--- Project3Admin/src/controller/AdminController.java
+++ Project3Admin/src/controller/AdminController.java
@@ -2,6 +2,10 @@ package controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,34 +13,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import po.Admin;
+import po.AdminStatus;
 import po.MessageStatus;
 import service.AdminManager;
+import service.AdminStatusManager;
 import service.MessageStatusManager;
 import service.impl.MessageStatusManagerImpl;
 
 @Controller
+@RequestMapping(value="admin")
 public class AdminController {
 
 	@Autowired
 	private AdminManager am;
+	@Autowired 
+	private AdminStatusManager asm;
+	
 	@Autowired
 	private MessageStatusManager msm;
 
-	@RequestMapping(value = "loginAdmin", method = RequestMethod.POST)
+	@RequestMapping(value = "login", method = RequestMethod.POST)
 	@ResponseBody
-	public MessageStatus login(Admin a) throws Exception {
+	public MessageStatus login(Admin a,HttpServletRequest request) throws Exception {
 		if (am.isExist(a)) {
 			Admin admin = am.findAdminByUsernameAndPassword(a);
-			if (admin != null)
+			if (admin != null) {
+				HttpSession ses = request.getSession();
+				ses.setAttribute("id", admin.getId());
 				return msm.createMessageStatus("success");
-			else
+			}	else
 				return msm.createMessageStatus("fail", "Incorrect Password");
 		} else
 			return msm.createMessageStatus("fail", "Username not found! Please make sure that username is correct!");
 	}
 
 	@RequestMapping(value = "logout")
-	public String logout() {
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.invalidate();
 		return "redirect:login.html";
 	}
 
@@ -55,4 +69,11 @@ public class AdminController {
 		return "Success";
 	}
 
+	@RequestMapping(value="updateStatus")
+	@ResponseBody
+	public MessageStatus updateStatus(AdminStatus as) {
+		asm.updateStatus(as);
+		return msm.createMessageStatus("success");
+	}
+	
 }
