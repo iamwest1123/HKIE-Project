23d63adb1abe105c2bfeb978b878d389c55ec1b3
Matthew Ma <matthew.ma@oocl.com>
2016-10-27 09:53:09 +0800

    complete merchant, customer, comment vo. get shop data for shopping page

--- Project3Customer/src/service/ShoppingManager.java
+++ Project3Customer/src/service/ShoppingManager.java
@@ -15,5 +15,5 @@ public interface ShoppingManager {
 	public boolean setShoppingCart(String merchantId, ShoppingCart cart, HttpSession session);
 	public List<Dish> getDish(String merchantId);
 	public MerchantVo getShopInfo(String merchantId);
-	public CommentVo getShopComment(String merchantId);
+	public List<CommentVo> getShopComment(String merchantId);
 }
