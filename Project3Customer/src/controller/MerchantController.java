b968c2a54d35ec6671db04609ca7917fc0290d07
Candy <candy.k.y.chan@oocl.com>
2016-10-27 09:09:02 +0800

    MerchantRegisterForm finished

--- Project3Customer/src/controller/MerchantController.java
+++ Project3Customer/src/controller/MerchantController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import po.Address;
 import po.Merchant;
 import service.MerchantManager;
 
@@ -17,8 +18,17 @@ public class MerchantController {
 	
 	@RequestMapping(value="addMerchant")
 	@ResponseBody
-	public Merchant addMerchart(Merchant m){
+	public Merchant addMerchart(Merchant m,String address1,String region1){
 	
+		System.out.println("........");
+		System.out.println(address1);
+		System.out.println("........");
+		System.out.println(region1);
+
+		Address add = new Address();
+		add.setAddress(address1);
+		add.setRegion(region1);
+		m.setAddress(add);
 		mm.registMerchant(m);
 		return m;
 	}
--- Project3Customer/src/service/impl/UserManagerImpl.java
+++ Project3Customer/src/service/impl/UserManagerImpl.java
@@ -1,7 +1,10 @@
 package service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +26,29 @@ public class UserManagerImpl implements UserManager{
 	}
 
 	@Override
+	@Transactional
 	public List<User> findAll() {
-		return ud.findAll();
+		
+		List<User> us=ud.findAll();
+		
+		List<User> us1= new ArrayList<User>();
+		for(User u:us){
+			User u1= new User();
+			try {
+//				BeanUtils.getProperty(u, "tels");
+				BeanUtils.copyProperties(u1, u);
+			} catch (BeansException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			us1.add(u1);
+			
+			
+		}
+		
+		//return ud.findAll();
+		return us1;
+		
 	}
 
 	@Override
