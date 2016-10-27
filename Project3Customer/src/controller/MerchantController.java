e5cd594875dab4d42f5c02c82639cbe9594064c7
RyanBB <KWONGKE@ZHA-ITA114-W7.corp.oocl.com>
2016-10-27 10:40:51 +0800

    customer homepage

--- Project3Customer/src/controller/MerchantController.java
+++ Project3Customer/src/controller/MerchantController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import po.Address;
 import po.Merchant;
 import service.MerchantManager;
+import vo.MerchantVo;
 
 @Controller
 @RequestMapping(value="merchant")
@@ -31,5 +37,11 @@ public class MerchantController {
 		m.setAddress(add);
 		mm.registMerchant(m);
 		return m;
+	}
+	
+	@RequestMapping(value="showMerchant")
+	@ResponseBody
+	public List<MerchantVo> showAllMerchants(){		
+		return mm.findAll();	
 	}
 }
