c980bc7c977284e256efb6763235974dcb7e6a4e
RyanBB <KWONGKE@ZHA-ITA114-W7.corp.oocl.com>
2016-10-27 15:53:44 +0800

    add link to adv page

--- Project3Customer/src/controller/AvertController.java
+++ Project3Customer/src/controller/AvertController.java
@@ -1,10 +1,5 @@
 package controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -12,16 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 import util.ProjectConstant;
-import vo.AdminAdvertVo;
 
 @Controller
 @RequestMapping(value="advert")
