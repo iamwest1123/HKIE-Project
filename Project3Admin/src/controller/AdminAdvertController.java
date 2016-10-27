745f934f9c76c22167cdf5a14f364d3ad4ec02a2
RyanBB <KWONGKE@ZHA-ITA114-W7.corp.oocl.com>
2016-10-27 13:34:31 +0800

    fake choose adv

---
+++ Project3Admin/src/controller/AdminAdvertController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import po.AdminAdvert;
+
+@Controller
+public class AdminAdvertController {
+
+	@RequestMapping(value = "loginAdmin", method = RequestMethod.POST)
+	@ResponseBody
+	public List<AdminAdvert> chooseAdvert(){
+		AdminAdvert ad1 = new AdminAdvert();
+		ad1.setMerchantId("4028b88157fffa970157fffa9b510000");
+		
+		AdminAdvert ad2 = new AdminAdvert();
+		ad2.setMerchantId("8a5eb8265802341101580234919f0000");
+		
+		AdminAdvert ad3 = new AdminAdvert();
+		ad2.setMerchantId("8a5e9d1c57ffe8ca0158001650710004");
+		
+		List<AdminAdvert> ads = new ArrayList<AdminAdvert>();
+		ads.add(ad1);
+		ads.add(ad2);
+		ads.add(ad3);
+		
+		return ads;		
+	}
+}
